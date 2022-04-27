12099368e91c3077e437925334d45487405c37d5
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-04-28 01:08:15 +0200

    Card Component & Upcoming Section done

--- src/components/EventsSection.vue
+++ src/components/EventsSection.vue
@@ -3,20 +3,10 @@
     <HeadingText :type="'white'" :icon="upcomingIcon">
       <template #title>Upcoming Events</template>
     </HeadingText>
-    <GridElement :grids="'3'">
-      <template #element-1>
-        <ContainerElement :contentPosition="'center center'">
-          <CardElement />
-        </ContainerElement>
-      </template>
-      <template #element-2>
-        <ContainerElement :contentPosition="'center center'">
-          <CardElement />
-        </ContainerElement>
-      </template>
-      <template #element-3>
-        <ContainerElement :contentPosition="'center center'">
-          <CardElement />
+    <GridElement :grids="upcomingCard.length.toString()">
+      <template v-for="card in upcomingCard" v-slot:[card.position]>
+        <ContainerElement :contentPosition="'center center'" :key="card.id">
+          <CardElement :content="card" />
         </ContainerElement>
       </template>
     </GridElement>
@@ -35,12 +25,14 @@ import GridElement from "./components_library/GridElement.vue";
 import ContainerElement from "./components_library/ContainerElement.vue";
 import CardElement from "./components_library/CardElement.vue";
 import ButtonElement from "./components_library/ButtonElement.vue";
+import upcomingCard from "../data/upcomingCard";
 
 export default {
   name: "EventsSection",
   data() {
     return {
       upcomingIcon,
+      upcomingCard,
     };
   },
   components: {
--- src/components/components_library/CardElement.vue
+++ src/components/components_library/CardElement.vue
@@ -4,24 +4,23 @@
       <PaperElement :styleVariant="'primary'">
         <ContainerElement :contentPosition="'center center'">
           <div>
-            <h3 class="card-title">Coaching Sessions</h3>
+            <h3 class="card-title">{{ content.title }}</h3>
             <p class="card-data">
-              <font-awesome-icon icon="fa-solid fa-calendar-days" />
-              24 Mar 18:00 PM
+              <font-awesome-icon :icon="content.infoTags[0].icon" />
+              {{ content.infoTags[0].info }}
             </p>
             <p class="card-description">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-              Vestibulum eget nunc eget nunc tincidunt euismod.
+              {{ content.description }}
             </p>
           </div>
         </ContainerElement>
       </PaperElement>
     </div>
-    <div class="card-button">
+    <div class="card-button" v-if="content.hasButton">
       <ContainerElement :contentPosition="'center center'">
         <ButtonElement :variant="'fourth'" :size="'xs'" :boxShadow="true">
-          <font-awesome-icon icon="fa-solid fa-plus" />
-          Find More
+          <font-awesome-icon :icon="content.button.icon" />
+          {{ content.button.text }}
         </ButtonElement>
       </ContainerElement>
     </div>
@@ -40,6 +39,12 @@ export default {
     ContainerElement,
     ButtonElement,
   },
+  props: {
+    content: {
+      type: Object,
+      required: true,
+    },
+  },
 };
 </script>
 
@@ -50,6 +55,7 @@ export default {
   border-radius: $mt-border-radius-xl;
   overflow: hidden;
   text-align: center;
+  box-shadow: $mt-shadow-default;
 
   &-title {
     font-size: $mt-font-size-sm;
